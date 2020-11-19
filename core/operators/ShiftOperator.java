c5c9d5bbf9fff809553b8189b9aab7dcddc2404a
Sreedhar Madivada <37278989+smadivad@users.noreply.github.com>
2020-11-19 07:15:09 -0600

    Create ShiftOperator.java

---
+++ core/operators/ShiftOperator.java
@@ -0,0 +1,24 @@
+/**
+ * @author sxm147
+ *
+ */
+public class ShiftOperator {
+	public static void main(String[] args) {
+
+		int a = 5;
+		int b = -10;
+
+		// left shift operator
+		// 0000 0101<<2 =0001 0100(20)
+		// similar to 5*(2^2)
+		System.out.println("a<<2 = " + (a << 2));
+
+		// right shift operator
+		// 0000 0101 >> 2 =0000 0001(1)
+		// similar to 5/(2^2)
+		System.out.println("b>>2 = " + (b >> 2));
+
+		// unsigned right shift operator
+		System.out.println("b>>>2 = " + (b >>> 2));
+	}
+}
