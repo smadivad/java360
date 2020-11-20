916e429d26dd986f8508d10878b7bccbfe9edc1e
Sreedhar Madivada <SMADIVAD@amfam.com>
2020-11-20 10:25:41 -0600

    eclipse project created

---
+++ src/main/java/com/sreeluck/operators/UnSignedShiftOperator.java
@@ -0,0 +1,18 @@
+package com.sreeluck.operators;
+/**
+ * 
+ */
+
+
+/**
+ * @author sxm147
+ *
+ */
+public class UnSignedShiftOperator {
+	public static void main(String args[]) {
+		int x = -1;
+		System.out.println(x >>> 29);
+		System.out.println(x >>> 30);
+		System.out.println(x >>> 31);
+	}
+}
